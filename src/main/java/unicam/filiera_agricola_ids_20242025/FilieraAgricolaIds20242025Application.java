54e977319359d572f94642e2a4a349988051c97c
mosconiluca <luca.mosconi@studenti.unicam.it>
2025-07-10 16:14:47 +0200

    Initial_commit

---
+++ src/main/java/unicam/filiera_agricola_ids_20242025/FilieraAgricolaIds20242025Application.java
@@ -0,0 +1,13 @@
+package unicam.filiera_agricola_ids_20242025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraAgricolaIds20242025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilieraAgricolaIds20242025Application.class, args);
+	}
+
+}
---
+++ src/test/java/unicam/filiera_agricola_ids_20242025/FilieraAgricolaIds20242025ApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.filiera_agricola_ids_20242025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilieraAgricolaIds20242025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
