ae96da918f59e56deed063f6b3811019e7dd13ab
iskos <iskandercherif241@gmail.com>
2025-09-19 16:37:52 +0200

    modifica Animatore + Venditore

--- src/main/java/unicam/filiera_agricola_ids_20242025/models/Utenti/Animatore.java
+++ src/main/java/unicam/filiera_agricola_ids_20242025/models/Utenti/Animatore.java
@@ -3,11 +3,14 @@ package unicam.filiera_agricola_ids_20242025.models.Utenti;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import unicam.filiera_agricola_ids_20242025.models.Evento;
+import unicam.filiera_agricola_ids_20242025.models.Invitato;
+import unicam.filiera_agricola_ids_20242025.models.OrganizzatoreInviti;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Animatore {
+public class Animatore implements OrganizzatoreInviti {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,12 +27,37 @@ public class Animatore {
     @OneToMany(cascade = CascadeType.ALL)
     private List<Evento> eventiCreati;
 
+    // Lista di osservatori (venditori)
+    @Transient
+    private List<Invitato> invitati = new ArrayList<>();
+
     public Animatore() {}
 
     public Animatore(Utente utente) {
         this.utente = utente;
         this.email = utente.getEmail();
     }
+
+    /* Observer pattern
+     Registra un nuovo osservatore (venditore) nella lista degli invitati.
+     In questo modo l'animatore sa chi deve notificare quando
+     viene organizzato un evento.*/
+
+    @Override
+    public void aggiungiInvitato(Invitato i) {
+        invitati.add(i);
+    }
+
+    // Questo metodo scorre tutti gli osservatori (venditori registrati) e invia loro l'invito
+    // chiamando il metodo riceviInvito() su ciascun oggetto Invitato.
+
+    @Override
+    public void notificaInvitati(Evento evento) {
+        for (Invitato i : invitati) {
+            i.riceviInvito(evento, this);
+        }
+    }
+
 
     public List<Evento> getEventiCreati() {
         return eventiCreati;
--- src/main/java/unicam/filiera_agricola_ids_20242025/models/Utenti/Venditori/Venditore.java
+++ src/main/java/unicam/filiera_agricola_ids_20242025/models/Utenti/Venditori/Venditore.java
@@ -2,13 +2,18 @@ package unicam.filiera_agricola_ids_20242025.models.Utenti.Venditori;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import unicam.filiera_agricola_ids_20242025.models.Evento;
+import unicam.filiera_agricola_ids_20242025.models.Invitato;
+import unicam.filiera_agricola_ids_20242025.models.Invito;
 import unicam.filiera_agricola_ids_20242025.models.Prodotti.Prodotto;
+import unicam.filiera_agricola_ids_20242025.models.Utenti.Animatore;
 import unicam.filiera_agricola_ids_20242025.models.Utenti.Utente;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public abstract class Venditore {
+public abstract class Venditore implements Invitato {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +34,10 @@ public abstract class Venditore {
     @JsonManagedReference
     private List<Prodotto> prodotti;
 
+    @OneToMany(mappedBy = "venditore", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Invito> invitiRicevuti = new ArrayList<>();
+
     public Venditore() {}
 
     public Venditore(Utente utente, String nome, Long pIva) {
@@ -36,6 +45,47 @@ public abstract class Venditore {
         this.nome = nome;
         this.pIva = pIva;
         this.email = utente.getEmail();
+    }
+
+    /* Observer pattern
+     Questo metodo viene chiamato dall'Animatore quando notifica un nuovo evento.
+     Crea un oggetto Invito associando il venditore (this), l'animatore e l'evento,
+     e lo aggiunge alla lista degli inviti ricevuti dal venditore.*/
+
+    @Override
+    public void riceviInvito(Evento evento, Animatore animatore) {
+        Invito invito = new Invito(this, animatore, evento);
+        invitiRicevuti.add(invito);
+    }
+
+    // Gestione inviti
+    public void accettaInvito(int idInvito) {
+        invitiRicevuti.stream()
+                .filter(invito -> invito.getIdInvito() == idInvito)
+                .findFirst()
+                .ifPresent(invito -> {
+                    invito.setStato("ACCETTATO");
+                    invitiRicevuti.remove(invito);
+                });
+    }
+
+    public void rifiutaInvito(int idInvito) {
+        invitiRicevuti.stream()
+                .filter(invito -> invito.getIdInvito() == idInvito)
+                .findFirst()
+                .ifPresent(invito -> {
+                    invito.setStato("RIFIUTATO");
+                    invitiRicevuti.remove(invito);
+                });
+    }
+
+
+    public List<Invito> getInvitiRicevuti() {
+        return invitiRicevuti;
+    }
+
+    public void setInvitiRicevuti(List<Invito> invito) {
+        this.invitiRicevuti = invito;
     }
 
     public String getNome() {
